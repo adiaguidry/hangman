f572a8424d8f9be022e2ed1da9a08bf626d6987a
adiaguidry <adiaguidry@gmail.com>
2023-02-02 13:02:07 -0700

    play function working need to finish fame over function

--- app.py
+++ app.py
@@ -10,20 +10,37 @@ def get_word():
 def play(word):
     word_completion = '_ ' * len(word)
     guessed = False
-    tries = 3
+    tries = 6
     guessed_letters = []
+    word_as_list = []
     print('Welcome, lets play hangman!')
     print(word_completion)
     while guessed == False and tries > 0:
-        letter_guessed = input('guess a letter: ').lower()
+        word_completion = ''
+        guess = input('guess a letter: ').lower()
         # check for only one letter and that it is a letter
-        if len(letter_guessed) == 1 and letter_guessed.isalpha():
-            guessed_letters += letter_guessed
-            word_completion = ''
-            for letter in word:
-                for item in guessed_letters:
-                    print(letter, item)
+        if len(guess) == 1 and guess.isalpha():
+            # check if already made that guess
+            if guess in guessed_letters:
+                print(f'You already made that guess {guess}')
+            # check if guess is in the word
+            elif guess not in word:
+                print(f'Sorry there is no {guess}')
+                guessed_letters += guess
+            else:
+                print(f'Yes, there is a/an {guess}')
+                guessed_letters += guess
+        for letter in word:
+            if letter in guessed_letters:
+                word_completion += letter
+            elif letter not in guessed_letters:
+                word_completion += '_'
         tries -= 1
+        print(word_completion, word)
+
+
+
+
 
 
 play(get_word())
