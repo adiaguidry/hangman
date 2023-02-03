e0d2d92a8f232dd9f267d28d9a5535e8bc182989
adiaguidry <adiaguidry@gmail.com>
2023-02-03 10:02:56 -0700

    updated guess word

--- app.py
+++ app.py
@@ -10,12 +10,11 @@ def get_word():
 def play(word):
     word_completion = '_ ' * len(word)
     guessed = False
-    tries = 6
+    tries = 10
     guessed_letters = []
-    word_as_list = []
     print('Welcome, lets play hangman!')
     print(word_completion)
-    while guessed == False and tries > 0:
+    while guessed is False and tries > 0:
         word_completion = ''
         guess = input('guess a letter: ').lower()
         # check for only one letter and that it is a letter
@@ -30,15 +29,24 @@ def play(word):
             else:
                 print(f'Yes, there is a/an {guess}')
                 guessed_letters += guess
+        if len(guess) == len(word) and guess.isalpha():
+            if guess == word:
+                print(f'yes you got it, the word is {guess}')
+                guessed = True
+            else:
+                print(f'no that was not the right word {guess}')
         for letter in word:
             if letter in guessed_letters:
                 word_completion += letter
             elif letter not in guessed_letters:
                 word_completion += '_'
         tries -= 1
-        print(word_completion, word)
-
-
+        print(word_completion)
+        if word_completion == word:
+            guessed = True
+            print('you got it')
+    if tries == 0 and word_completion is not word:
+        print('lost, no more tires')
 
 
 
